5eae244c69bd32512568382edd9236460d7a8eb2
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2022-11-08 17:57:27 +0600

    image path updated

--- src/utils/etc.js
+++ src/utils/etc.js
@@ -1,5 +1,5 @@
 // export const imgdestination = `../../../jwt-auth-practice/build/images/`;
-export const imgdestination = `/images/`;
+export const imgdestination = `./images/`;
 
 export const placeHolderImage = `important/profileplaceholder.jpg`;
 

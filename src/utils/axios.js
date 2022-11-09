b9542cb0a5c2bfa57523fbd187273e2c57dfc79f
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2022-11-09 16:15:35 +0600

    updated axios env conditions

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const GetURL = () =>
-  process.env.REACT_APP_MODE === "development"
-    ? process.env.REACT_APP_LOCAL_URL
-    : process.env.REACT_APP_HOSTING_URL;
+  process.env.REACT_APP_MODE === "production"
+    ? process.env.REACT_APP_HOSTING_URL
+    : process.env.REACT_APP_LOCAL_URL;
 
 export const request = axios.create({
   baseURL: GetURL(),
